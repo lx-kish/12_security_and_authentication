28e7b11772d178f2043ed36c02cdd27e386a193c
lx kish <urban.ktulhu@gmail.com>
2018-08-14 12:41:48 +1200

    131 Hashing passwords

--- server/models/user.js
+++ server/models/user.js
@@ -1,17 +1,39 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const SALT_I = 10;
 
 const userSchema = mongoose.Schema({
-    email:{
-        type:String,
-        required:true,
-        trim:true,
-        unique:1
+    email: {
+        type: String,
+        required: true,
+        trim: true,
+        unique: 1
     },
-    password:{
-        type:String,
-        required:true,
-        minlength:6
+    password: {
+        type: String,
+        required: true,
+        minlength: 6
     }
+})
+
+userSchema.pre('save', function (next) {
+    var user = this;
+
+    if (user.isModified('password')) {
+        bcrypt.genSalt(SALT_I, function (err, salt) {
+            if (err) return next(err);
+
+            bcrypt.hash(user.password, salt, function (err, hash) {
+                if (err) return next(err);
+
+                user.password = hash;
+                next();
+            })
+        })
+    } else {
+        next();
+    }
+
 })
 
 const User = mongoose.model('User', userSchema)
